8d124d3bd4c945040e17c5a08e410b5b374284d2
agafia-zer <agafiazers@gmail.com>
2024-06-23 23:09:13 +0300

    новый вывод вопросов

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import java.sql.Connection;
 import java.text.SimpleDateFormat;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -26,28 +25,23 @@ public class Main {
                 if (cardResponse.equalsIgnoreCase("Да")) {
                     ClientManager.createCard(scanner, connection, clientCache, cardsCache);
                 }
-            }  else if (response.equalsIgnoreCase("Нет")) {
+            } else if (response.equalsIgnoreCase("Нет")) {
                 System.out.println("Добавить карту для существующего клиента? (Да/Нет)");
-                String deleteResponse = scanner.nextLine();
-                if (deleteResponse.equalsIgnoreCase("Да")) {
-
+                String cardResponse = scanner.nextLine();
+                if (cardResponse.equalsIgnoreCase("Да")) {
                     ClientManager.createCard(scanner, connection, clientCache, cardsCache);
+                } else if (cardResponse.equalsIgnoreCase("Нет")) {
+                    System.out.println("Удалить карту существующего клиента? (Да/Нет)");
+                    String deleteResponse = scanner.nextLine();
+                    if (deleteResponse.equalsIgnoreCase("Да")) {
+                        ClientManager.cancelCard(scanner, connection, cardsCache);
+                    } else if (deleteResponse.equalsIgnoreCase("Нет")) {
+                        break; // Выход из цикла, если пользователь не хочет удалять карту
+                    }
                 }
-                break;
-            }else if (response.equalsIgnoreCase("Нет")) {
-                System.out.println("Удалить карту существующего клиента? (Да/Нет)");
-                String deleteResponse = scanner.nextLine();
-                if (deleteResponse.equalsIgnoreCase("Да")) {
-                    ClientManager.cancelCard(scanner, connection, cardsCache);
-                }
-                break;
             } else {
                 System.out.println("Некорректный ввод! Введите 'Да' или 'Нет'");
             }
         }
-
-
-
-        scanner.close();
     }
 }
